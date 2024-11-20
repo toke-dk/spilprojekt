c65c1a3eccc0bc5caf0678944649cf67d4f70ae0
Toke Friis <tokefriis@gmail.com>
2024-11-20 10:40:37 +0100

    refactor collumn and row count

--- spilprojekt.ino
+++ spilprojekt.ino
@@ -5,9 +5,12 @@ int latchPin = 7;
 int dataPin = 8;
 int clockPin = 9;
 
-const byte rows[8] = {0b11111110,0b11111101,0b11111011,0b11110111,0b11101111,0b11011111,0b10111111,0b01111111};
+const int rowCount = 8;
+const int collumnCount = 8;
 
-byte frame[8] = { 0b00000001,
+const byte rows[rowCount] = {0b11111110,0b11111101,0b11111011,0b11110111,0b11101111,0b11011111,0b10111111,0b01111111};
+
+byte frame[collumnCount] = { 0b00000001,
                   0b00000011,0b00000111,0b00001111,0b00011111,0b00111111,0b01111111,0b11111111};
 
 Position ballPosition(2,2);
@@ -30,14 +33,13 @@ void loop() {
 
 void loadFrame() {
   
-  for (int i = 0; i<8; i++) {
+  for (int i = 0; i<collumnCount; i++) {
     digitalWrite(latchPin, LOW);
     
     // gå igennem rækkerne
     shiftOut(dataPin, clockPin, LSBFIRST, rows[i]);
-    //Serial.println("done");
 
-    // tilføj din nuværende række
+    // tilføj din nuværende kolonne
     shiftOut(dataPin, clockPin, LSBFIRST, frame[i]);
     
     
